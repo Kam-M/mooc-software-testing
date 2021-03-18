8436e7ba45deb2f6ac2357d79f5fd38c26d354c8
Kam-M <kamil.marcol@gmail.com>
2021-03-18 18:37:56 +0100

    test classes for GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,47 @@
 package tudelft.ghappy;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.hamcrest.core.IsNot.*;
+
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+	
+	private GHappy gHappy;
+	
+	@BeforeEach
+	void initialize() {
+		this.gHappy = new GHappy();
+	}
+	
+	@Test
+	void shouldReturnTrueIfGIsHappy() {
+		boolean isHappy = gHappy.gHappy("xxxggxx");
+		assertTrue(isHappy);
+	}
+	
+	@Test
+	void shouldReturnFalseIfOnlyOneGIsHappy() {
+		boolean isHappy = gHappy.gHappy("xxxggxgx");
+		assertThat(isHappy, not(true));
+	}
+	
+	@Test
+	void shouldReturnFalseIsOnlyOneG() {
+		boolean isHappy = gHappy.gHappy("xxxxgx");
+		assertFalse(isHappy);
+	}
+	
+	@Test
+	void shouldReturnFalseIfTwoGButNotNearly() {
+		boolean isHappy = gHappy.gHappy("xxgxxgx");
+		assertFalse(isHappy);
+	}
+	
 }
